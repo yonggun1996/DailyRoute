6dda0de59cfce01097d9908775a87d6c037c4e93
박용건 <dragon96@golfzon.com>
2024-10-27 15:24:43 +0900

    검색바 생성

--- app/src/main/java/com/example/dailyroute/MainActivity.kt
+++ app/src/main/java/com/example/dailyroute/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.dailyroute
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.dailyroute.common.CommonUI
 import com.example.dailyroute.ui.theme.DailyRouteTheme
 
@@ -19,11 +24,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             DailyRouteTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                Column (
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     CommonUI.AppHeader()
+                    CommonUI.SearchBar(defalutText = "역 이름을 입력해주세요")
                 }
             }
         }
@@ -42,6 +51,16 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     DailyRouteTheme {
-        CommonUI.AppHeader()
+        // A surface container using the 'background' color from the theme
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CommonUI.AppHeader()
+            CommonUI.SearchBar(defalutText = "역 이름을 입력해주세요")
+        }
     }
 }
--- app/src/main/java/com/example/dailyroute/common/AppHeader.kt
+++ app/src/main/java/com/example/dailyroute/common/AppHeader.kt
@@ -3,22 +3,37 @@ package com.example.dailyroute.common
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 object CommonUI {
+
+    /*
+     * 기본적으로 자주 사용하고 있는 ui를 공통적으로 사용하기 위해 해당 object에 생성
+     */
+
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun AppHeader() {
@@ -41,5 +56,43 @@ object CommonUI {
 //            backgroundColor = MaterialTheme.colors.primary, // 앱 바 배경색 설정
 //            contentColor = Color.White // 텍스트와 아이콘 색상 설정
         )
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun SearchBar(defalutText: String) {
+        var query by remember { mutableStateOf("") } // 검색어 관리 변수
+        var active by remember { mutableStateOf(false) } // 검색 바 활성화 상태
+
+        // material 디자인에서 제공하는 검색바 사용
+        androidx.compose.material3.SearchBar(
+            query = query,
+            onQueryChange = { query = it }, // 검색어가 변경될 때 마다 호출
+            onSearch = { /* 검색 로직 추가 */ }, // 검색이 실행될 때 호출
+            active = active, // 검색 바의 활성화 상태를 나타냅니다.
+            onActiveChange = { active = it }, // 검색 바의 활성화 상태가 변경될 때 호출됩니다.
+            placeholder = { Text(text = defalutText) },
+            leadingIcon = { Icon(Icons.Default.Search, contentDescription = "검색") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+
+        }
+
+//        SearchBar(
+//            query = query,
+//            onQueryChange = { query = it },
+//            onSearch = { /* 검색 로직 추가 */ },
+//            active = active,
+//            onActiveChange = { active = it },
+//            placeholder = { Text(defalutText) },
+//            leadingIcon = { Icon(Icons.Default.Search, contentDescription = "검색") },
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .padding(16.dp)
+//        ) {
+//            // 검색어와 관련된 추천 목록 표시 등 추가 UI를 여기에 작성할 수 있습니다
+//        }
     }
 }
